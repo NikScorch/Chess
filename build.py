0d457f08f5977c1661eb3c55aa67e4799e7e5cc3
Cormac Shannon <cormacpshannon@gmail.com>
2023-03-21 17:56:12 +0000

    updating build system

---
+++ build.py
@@ -0,0 +1,28 @@
+import PyInstaller.__main__
+import os
+
+try:
+    import pygame
+except ImportError:
+    print("Pygame is not installed and is required to build")
+    print("To install, please run \"python -m pip -r requirements.txt\"")
+
+name = ""
+if os.name == "nt":
+    name = "chess.exe"
+elif os.name == "posix":
+    name = "chess"
+
+PyInstaller.__main__.run([
+    'run.pyw',
+    '--onefile',
+    '--windowed',
+    '-n' + name,
+    "--distpath", ".",
+    "--clean",
+    "--log-level", "FATAL"
+])
+
+print("\n" + "="*50)
+print("Compiled as \"" + name + "\" in current working directory")
+print("The \"./build\" directory can be deleted safely")
