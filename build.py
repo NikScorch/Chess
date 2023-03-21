018bc32600cadf720d2ea325667481a50f321bbb
Cormac Shannon <cormacpshannon@gmail.com>
2023-03-21 23:13:45 +0000

    added archiving

--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 try:
     import PyInstaller.__main__
 except ImportError:
@@ -20,17 +21,68 @@ if os.name == "nt":
 elif os.name == "posix":
     name = "chess"
 
-PyInstaller.__main__.run([
-    'run.pyw',
-    '--onefile',
-    '--windowed',
-    '-n' + name,
-    '-i', 'icon.ico',
-    "--distpath", ".",
-    "--clean",
-    "--log-level", "FATAL"
-])
+# PyInstaller.__main__.run([
+#     'run.pyw',
+#     '--onefile',
+#     '--windowed',
+#     '-n' + name,
+#     '-i', 'icon.ico',
+#     "--distpath", ".",
+#     "--clean",
+#     "--log-level", "FATAL"
+# ])
 
 print("\n" + "="*50)
 print("Compiled as \"" + name + "\" in current working directory")
 print("The \"./build\" directory can be deleted safely")
+
+
+import sys
+if sys.argv.__len__() > 1:
+    if sys.argv[-1] == "--zip":
+        import zipfile 
+        from shutil import rmtree
+
+        if ".mkzip" in os.listdir():
+            rmtree(".mkzip")
+
+        def remove_hidden(files):
+            dotless = []
+            for file in files:
+                if file[0] == ".":
+                    continue
+                dotless.append(file)
+            return dotless
+        all_files = remove_hidden(os.listdir())
+        art_files = all_files.copy()
+        art_files.remove("web")
+
+        with open(".gitignore", "r") as fp:
+            gitignore = fp.read().split("\n")
+            for i in gitignore:
+                if i in art_files:
+                    art_files.remove(i)
+        
+        rep_files = "web"
+
+        os.mkdir(".mkzip")
+        os.mkdir(".mkzip/Artefact")
+        
+        for file in art_files:
+            if "." in file:
+                os.symlink("../../{}".format(file), ".mkzip/Artefact/{}".format(file), target_is_directory=False)
+            else:
+                os.symlink("../../{}".format(file), ".mkzip/Artefact/{}".format(file), target_is_directory=True)
+        
+        os.symlink("../{}".format(rep_files), ".mkzip/Report", target_is_directory=True)
+
+        def zipdir(path, ziph):
+            # ziph is zipfile handle
+            for root, dirs, files in os.walk(path, followlinks=True):
+                for file in files:
+                    ziph.write(os.path.join(root, file), 
+                            os.path.relpath(os.path.join(root, file)))#, 
+                                            #os.path.join(path, '..')))
+        os.chdir(".mkzip/")
+        with zipfile.ZipFile('../2023-219-EV-154279.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+            zipdir('.', zipf)
